f6a552836de54540f0517d1233a7d8138803af5b
Timsy Magnus <tmherron09@gmail.com>
2020-08-19 11:51:10 -0500

    Init Commit

---
+++ TrashCollection/Models/Customer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrashCollection.Models
+{
+    public class Customer
+    {
+
+        [Key]
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public int PhoneNumber { get; set; }
+
+
+        public string WeeklyPickupDay { get; set; }
+        public bool SpecialtyPickupPending { get; set; }
+
+
+
+
+        public double OutstandingBalance { get; set; }
+
+        public string StartDate { get; set; }
+        public string EndDate { get; set; }
+
+
+        public string Address { get; set; }
+        public string ZipCode { get; set; }
+
+
+
+
+    }
+}
---
+++ TrashCollection/Models/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrashCollection.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+
+
+        // Change to Zone/Trash Route
+        public string ZipCode { get; set; }
+
+
+
+
+    }
+}
