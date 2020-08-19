2fd422f4dd946d72a2ed2dd95aa7ccbcfb1c2d94
Timsy Magnus <tmherron09@gmail.com>
2020-08-19 17:25:39 -0500

    Work on Customer Model and Add CustomersController scaffold

---
+++ TrashCollection/Controllers/CustomersController.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TrashCollection.Data;
+using TrashCollection.Models;
+
+namespace TrashCollection.Controllers
+{
+    [Authorize(Roles = "Customer")]
+    public class CustomersController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public CustomersController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // Get: Finish Registration
+        public IActionResult FinishRegistration()
+        {
+            Customer customer = new Customer();
+            var userId = this.User.FindFirstValue(ClaimTypes.NameIdentifier);
+            customer.IdentityUserId = userId;
+
+
+
+
+
+
+            return View();
+        }
+
+        // POST: Finish Registering
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult FinishRegistration(IFormCollection collection)
+        {
+            try
+            {
+
+
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CustomersController
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+
+        // GET: CustomersController/Details/5
+        public IActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: CustomersController/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CustomersController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                
+
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CustomersController/Edit/5
+        public IActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        public IActionResult AddAccountDetails()
+        {
+            return View();
+        }
+
+
+        // POST: CustomersController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CustomersController/Delete/5
+        public IActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: CustomersController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
--- TrashCollection/Models/Customer.cs
+++ TrashCollection/Models/Customer.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -10,30 +11,44 @@ namespace TrashCollection.Models
     public class Customer
     {
 
+        public Customer()
+        {
+            OutstandingBalance = 0.00;
+        }
+
         [Key]
         public int Id { get; set; }
+        [Required]
         public string FirstName { get; set; }
-        public string LastName { get; set; }
+        [Required]
+        public string FamilyName { get; set; }
+        [Required]
         public string EmailAddress { get; set; }
+        [Required]
         public int PhoneNumber { get; set; }
 
 
         public string WeeklyPickupDay { get; set; }
-        public bool SpecialtyPickupPending { get; set; }
+        public bool SpecialtyPickupCompleted { get; set; }
 
 
 
 
         public double OutstandingBalance { get; set; }
 
+
         public string StartDate { get; set; }
         public string EndDate { get; set; }
 
-
+        [Required]
         public string Address { get; set; }
+        [Required]
         public string ZipCode { get; set; }
 
 
+        [ForeignKey("IdentityUser")]
+        public string IdentityUserId { get; set; }
+        public IdentityUser IdentityUser { get; set; }
 
 
     }
